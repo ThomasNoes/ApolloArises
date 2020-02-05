eb94acb952fb6df4d8fad860fcdfcb23905218d2
noesmos <tnm@live.dk>
2020-02-05 09:19:52 +0100

    moved scripts

---
+++ MazeGeneration/Assets/Scripts/NDC/ElevatorMovement.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorMovement : MonoBehaviour
+{
+    public Vector3 elevatorVelocity;
+    public float finalElevatorHeight;
+    private bool goingDown = false;
+    private bool goingUp = false;
+    private float renderIntensity;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderIntensity = RenderSettings.ambientIntensity;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetKeyDown("b"))
+        {
+            StartCoroutine("MoveDown");
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            StartCoroutine("MoveUp");
+        } */
+
+
+    }
+
+    public void DownBtn()
+    {
+        if (GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs == 5 && !goingDown && !goingUp)
+        {
+            StartCoroutine("MoveDown");
+        }
+        else
+        {
+            Debug.Log("Puzzle not done!! " + GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs + "/5 plugs are placed correctly.");
+            // Puzzle not solved
+            // Some sound feedback or something??
+        }
+    }
+
+    public void UpBtn()
+    {
+
+        if (GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs == 5 && !goingDown && !goingUp)
+        {
+            StartCoroutine("MoveUp");
+        }
+        else
+        {
+            Debug.Log("Puzzle not done!! " + GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs + "/5 plugs are placed correctly.");
+            // Puzzle not solved
+            // Some sound feedback or something??
+        }
+    }
+
+    public IEnumerator MoveDown()
+    {
+
+        goingUp = false;
+        StopCoroutine("goingUp");
+        goingDown = true;
+        GameObject.FindObjectOfType<AudioManager>().Play("ElevatorRunningSound");
+        while (transform.position.y > finalElevatorHeight && goingDown)
+        {
+            transform.position = transform.position + elevatorVelocity * Time.fixedDeltaTime;
+            player.transform.position = new Vector3(player.transform.position.x, transform.position.y + elevatorVelocity.y * Time.fixedDeltaTime, player.transform.position.z);
+            yield return new WaitForSeconds(0f);
+        }
+        goingDown = false;
+        RenderSettings.ambientIntensity = 0;
+
+    }
+
+    public IEnumerator MoveUp()
+    {
+        if (GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs == 5)
+        {
+            goingDown = false;
+            StopCoroutine("goingDown");
+            goingUp = true;
+            GameObject.FindObjectOfType<AudioManager>().Play("ElevatorRunningSound");
+            while (transform.position.y < 0 && goingUp)
+            {
+                transform.position = transform.position - elevatorVelocity * Time.fixedDeltaTime;
+                player.transform.position = new Vector3(player.transform.position.x, transform.position.y + elevatorVelocity.y * Time.fixedDeltaTime, player.transform.position.z);
+                yield return new WaitForSeconds(0f);
+            }
+            goingUp = false;
+            RenderSettings.ambientIntensity = 1;
+        }
+        else
+        {
+            // Puzzle not solved
+            // Some sound feedback or something??
+            Debug.Log("Puzzle not done!! " + GameObject.FindGameObjectWithTag("FuseBox").GetComponent<FuseBoxPuzzle>().correctPlugs + "/5 plugs are placed correctly.");
+        }
+    }
+}
---
+++ MazeGeneration/Assets/Scripts/NDC/FusePlugHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FusePlugHandler : MonoBehaviour
+{
+    private bool inSocket = false;
+    [HideInInspector]
+    public GameObject socketGoIn;
+    [HideInInspector]
+    public GameObject socketGoOut;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inSocket)
+        {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.GetComponent<fuseSocketHandler>().occupiedSocket)
+        {
+            return;
+        }
+
+        inSocket = true;
+        socketGoIn = other.gameObject;
+
+        transform.parent.GetComponent<Valve.VR.InteractionSystem.Hand>().DetachObject(gameObject);
+
+        gameObject.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, other.transform.position.z);
+
+        EventCallbacks.FusePuzzlePlugIn fpPlugIn = new EventCallbacks.FusePuzzlePlugIn();
+        fpPlugIn.sGo = socketGoIn;
+        fpPlugIn.go = gameObject;
+        fpPlugIn.FireEvent();
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        inSocket = false;
+        socketGoOut = other.gameObject;
+
+        EventCallbacks.FusePuzzlePlugOut fpPlugOut = new EventCallbacks.FusePuzzlePlugOut();
+        fpPlugOut.sGo = socketGoOut;
+        fpPlugOut.go = gameObject;
+        fpPlugOut.FireEvent();
+
+    }
+}
