8c3a61cefc76cc3c0257f7ee6fa19ec62a6a8e84
Jakob <jakob.hust@gmail.com>
2020-02-06 11:46:41 +0100

    fixing .gitignore

--- MazeGeneration/Library/PackageCache/com.unity.memoryprofiler@0.1.0-preview.5/Editor/Database/Soa/DataSource.cs
+++
@@ -1,7 +0,0 @@
-namespace Unity.MemoryProfiler.Editor.Database.Soa
-{
-    internal abstract class DataSource<DataT>
-    {
-        public abstract void Get(Range range, ref DataT[] dataOut);
-    }
-}
