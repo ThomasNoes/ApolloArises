28b19ab57b9bc0338424fad8583105ee964375c1
Jakob <jakob.hust@gmail.com>
2020-02-07 14:12:10 +0100

    Update CamPosSwitcher.cs

--- MazeGeneration/Assets/Scripts/Camera/CamPosSwitcher.cs
+++ MazeGeneration/Assets/Scripts/Camera/CamPosSwitcher.cs
@@ -1,19 +1,26 @@
-﻿namespace MazeGeneration.Camera
+﻿namespace Assets.Scripts.Camera
 {
     using UnityEngine;
 
     public class CamPosSwitcher : MonoBehaviour
     {
-        private GameObject thisCamera;
-        private GameObject player;
-        // private GameObject[] 
+        public float rayMaxDist = 20.0f;
+        private GameObject thisCamera, player, currentNextPortal, currentPrevPortal;    // TODO find and assign current portal pairs for each maze
+        private FollowCam followCam;
+        private LayerMask layerMask;
+        private bool prevCollision = false, nextCollision = false;
 
         private void Start()
         {
+            layerMask = LayerMask.GetMask("Player");
+            layerMask |= LayerMask.GetMask("Ignore Raycast");
+            layerMask = ~layerMask;
+
             thisCamera = gameObject;
+            followCam = thisCamera.GetComponent<FollowCam>();
             player = GameObject.FindGameObjectWithTag("Player");
 
-            if (player != null)
+            if (player != null && followCam != null)
                 InvokeRepeating("CheckerLoop", 2.0f, 0.3f);
         }
 
@@ -23,13 +30,67 @@
         /// <param name="dir">0 = previous maze, 1 = next maze</param>
         public void PositionSwitch(int dir)
         {
+            if (dir == 0)
+            {
+                followCam.offset = -followCam.offset;   // TODO: Find a proper way to do this
+            }
+            else if (dir == 1)
+            {
+                followCam.offset = +followCam.offset;   // TODO: Find a proper way to do this
+            }
+        }
 
+        private void ResetBools()
+        {
+            nextCollision = false;
+            prevCollision = false;
         }
 
         private void CheckerLoop()
         {
-            // if ()
+            Vector3 dirPrevPortal =  - thisCamera.transform.position;
+            Vector3 dirNextPortal = -thisCamera.transform.position; // TODO find portal edge positions (+- center?)
 
+            RaycastHit hit;
+
+            if (Physics.Raycast(thisCamera.transform.position, dirNextPortal, out hit, rayMaxDist, layerMask))
+            {
+                if (hit.collider.tag == currentNextPortal.tag) // TODO: Make sure portals are tagged
+                {
+                    nextCollision = true;
+                }
+            }
+            if (Physics.Raycast(thisCamera.transform.position, dirPrevPortal, out hit, rayMaxDist, layerMask))
+            {
+                if (hit.collider.tag == currentPrevPortal.tag) // TODO: Make sure portals are tagged
+                {
+                    prevCollision = true;
+                }
+            }
+
+            if (nextCollision && prevCollision)
+            {
+                if (Vector3.Distance(thisCamera.transform.position, currentNextPortal.transform.position) <
+                    Vector3.Distance(thisCamera.transform.position, currentPrevPortal.transform.position))  // TODO: find a better solution than distance
+                {
+                    PositionSwitch(1);
+                }
+                else
+                {
+                    PositionSwitch(0);
+                }
+                ResetBools();
+            }
+            else if (nextCollision)
+            {
+                PositionSwitch(1);
+                ResetBools();
+            }
+            else if (prevCollision)
+            {
+                PositionSwitch(0);
+                ResetBools();
+            }
 
             // TODO: Make sure that first and end maze behaviour is different YYY
         }
