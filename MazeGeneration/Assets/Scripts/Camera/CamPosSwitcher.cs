9af3d67b90c0f02916a567ac8a7c323a8a295a7b
Jakob <jakob.hust@gmail.com>
2020-02-07 13:29:55 +0100

    Update

---
+++ MazeGeneration/Assets/Scripts/Camera/CamPosSwitcher.cs
@@ -0,0 +1,37 @@
+﻿namespace MazeGeneration.Camera
+{
+    using UnityEngine;
+
+    public class CamPosSwitcher : MonoBehaviour
+    {
+        private GameObject thisCamera;
+        private GameObject player;
+        // private GameObject[] 
+
+        private void Start()
+        {
+            thisCamera = gameObject;
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player != null)
+                InvokeRepeating("CheckerLoop", 2.0f, 0.3f);
+        }
+
+        /// <summary>
+        /// Switches camera position between previous and next maze
+        /// </summary>
+        /// <param name="dir">0 = previous maze, 1 = next maze</param>
+        public void PositionSwitch(int dir)
+        {
+
+        }
+
+        private void CheckerLoop()
+        {
+            // if ()
+
+
+            // TODO: Make sure that first and end maze behaviour is different YYY
+        }
+    }
+}
