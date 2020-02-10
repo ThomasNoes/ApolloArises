0ea2dc721e285fb38552fdfbaf2a1ad1684b260c
Jakob <jakob.hust@gmail.com>
2020-02-10 13:42:23 +0100

    Update

---
+++ MazeGeneration/Assets/Scripts/Interactable/InteractableObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+namespace Assets.Scripts.Interactable
+{
+    using UnityEngine;
+
+    [RequireComponent(typeof(OVRGrabbable), typeof(Rigidbody))]
+    public class InteractableObject : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(InteractableObject))]
+    public class Interactable_Editor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+
+            DrawDefaultInspector();
+
+            var script = target as InteractableObject;
+
+            //if (script.gameObject.layer != "Interactable")
+            //    //script.gameObject.tag = "Interactable";
+
+        }
+    }
+#endif
+}
