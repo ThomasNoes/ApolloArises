21c317d6c9e73a2084c7e0e8e40afb9e5e9626bd
noesmos <tnm@live.dk>
2020-02-14 12:51:24 +0100

    debug player controller

--- MazeGeneration/Assets/Scripts/DebugMovement.cs
+++ MazeGeneration/Assets/Scripts/DebugMovement.cs
@@ -1,54 +1,152 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class DebugMovement : MonoBehaviour
 {
-    CharacterController characterController;
+    Rigidbody rb;
+    Transform cam;
 
     public float speed = 6.0f;
     public float rotSpeed = 50.0f;
     public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
+
+    bool useWorldSpace = false;
+
+    Vector3 playerForward;
+    Vector3 playerRight;
 
     private Vector3 moveDirection = Vector3.zero;
 
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+        
+
+
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(0);
     }
 
-    void LateUpdate()
+    private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.E))
+        //switch movement mode
+        if (Input.GetKeyDown("space"))
         {
-            transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0, -rotSpeed * Time.deltaTime, 0);
+
+            useWorldSpace = !useWorldSpace;
         }
 
-        if (characterController.isGrounded)
+        playerForward = Vector3.ProjectOnPlane(cam.forward, transform.up).normalized;
+        playerRight = Vector3.ProjectOnPlane(cam.right, transform.up).normalized;
+
+
+        movement();
+
+        turning();
+
+
+
+
+    }
+
+    private void movement()
+    {
+
+        //moving 
+        if (Input.GetKey("w")) //forward
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
+            if (useWorldSpace)
             {
-                moveDirection.y = jumpSpeed;
+                rb.AddForce(Vector3.forward * speed);
+            }
+            else
+            {
+                rb.AddForce(playerForward * speed);
             }
         }
+        if (Input.GetKey("s")) //backward
+        {
+            if (useWorldSpace)
+            {
+                rb.AddForce(-Vector3.forward * speed);
+            }
+            else
+            {
+                rb.AddForce(-playerForward * speed);
+            }
+        }
+        if (Input.GetKey("a")) // leftward
+        {
+            if (useWorldSpace)
+            {
+                rb.AddForce(-Vector3.right * speed);
+            }
+            else
+            {
+                rb.AddForce(-playerRight * speed);
+            }
+        }
+        if (Input.GetKey("d")) //rightward
+        {
+            if (useWorldSpace)
+            {
+                rb.AddForce(Vector3.right * speed);
+            }
+            else
+            {
+                rb.AddForce(playerRight * speed);
+            }
+        }
+    }
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+    private void turning()
+    {
+        //turning
+        if (Input.GetKey("q")) // leftward
+        {
+            transform.Rotate(0, -rotSpeed * Time.deltaTime, 0);
 
-        // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+
+        }
+        if (Input.GetKey("e")) //rightward
+        {
+
+            transform.Rotate(0, rotSpeed * Time.deltaTime, 0);
+
+        }
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = cam.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            cam.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            cam.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            cam.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
     }
 }
