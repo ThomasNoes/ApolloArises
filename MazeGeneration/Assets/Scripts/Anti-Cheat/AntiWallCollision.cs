2ad12cab654dbfd19569df6f28f71a53d7593dbe
Jakob <jakob.hust@gmail.com>
2020-02-19 14:45:55 +0100

    Anti cheat feedback added

--- MazeGeneration/Assets/Scripts/Anti-Cheat/AntiWallCollision.cs
+++ MazeGeneration/Assets/Scripts/Anti-Cheat/AntiWallCollision.cs
@@ -4,7 +4,7 @@ public class AntiWallCollision : MonoBehaviour
 {
     private void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.CompareTag("Wall"))
+        if (col.gameObject.CompareTag(Camera.main.tag))
         {
             OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
         }
@@ -12,7 +12,7 @@ public class AntiWallCollision : MonoBehaviour
 
     private void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.CompareTag("Wall"))
+        if (col.gameObject.CompareTag(Camera.main.tag))
         {
             OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
         }
