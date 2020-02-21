cf7f25dd89c29a810177672599ff2e3712fd03e0
Jakob <jakob.hust@gmail.com>
2020-02-21 13:31:28 +0100

    Update

---
+++ MazeGeneration/Assets/Scripts/Debugging/CenterObjectToPlayArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterObjectToPlayArea : MonoBehaviour
+{
+    private Vector3 playAreaCenter;
+
+    void Start()
+    {
+        Vector3[] playAreaLocation = OVRManager.boundary.GetGeometry(OVRBoundary.BoundaryType.PlayArea);
+
+        Debug.Log("geo first vector: " + playAreaLocation[0]);
+        Debug.Log("Play area size: " + OVRManager.boundary.GetDimensions(OVRBoundary.BoundaryType.PlayArea));
+    }
+
+}
