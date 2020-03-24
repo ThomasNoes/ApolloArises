66c0ec1fd0986140a5526c0ad2425161ea731621
noesmos <tnm@live.dk>
2020-03-24 12:45:37 +0100

    button done

---
+++ MazeGeneration/Assets/Events.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+    public AudioSource audio;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void OnCustomButtonPress()
+    {
+        Debug.Log("We pushed our custom button!");
+
+        audio.Play();
+    }
+}
