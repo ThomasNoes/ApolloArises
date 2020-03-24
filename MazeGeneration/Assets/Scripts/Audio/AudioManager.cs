8564342977fdd2a4b759db331c1f7a3124bc1ea0
Heraji <jakob.hust@gmail.com>
2020-03-24 12:02:10 +0100

    Audio manager + deleted SteamVR folders

--- MazeGeneration/Assets/Scripts/Audio/AudioManager.cs
+++ MazeGeneration/Assets/Scripts/Audio/AudioManager.cs
@@ -1,64 +1,178 @@
-using UnityEngine.Audio;
-using System;
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
+    public SoundEvent[] soundEvents;
+    private AudioSource thisAudioSource;
 
-	public Sound[] sounds;
-    float currentTime;
-    public float delayTime = 4;
-    string delayedSound;
-    bool timerActive = false;
-
-	void Awake()
-	{
-
-		foreach (Sound s in sounds)
-		{
-			s.source = gameObject.AddComponent<AudioSource>();
-			s.source.clip = s.clip;
-			s.source.loop = s.loop;
-
-		}
-
-        Play("AmbientSound");
+    private void Awake()
+    {
+        thisAudioSource = GetComponent<AudioSource>();
+        thisAudioSource.playOnAwake = false;
     }
 
-    private void Update()
+    private void Start()
     {
-        if (timerActive)
-        {
-            if (delayTime <= Time.time - currentTime)
+        if (soundEvents != null)
+            for (int i = 0; i < soundEvents.Length; i++)
             {
-                timerActive = false;
-                Play(delayedSound);
+                soundEvents[i].thisAudioManager = this;
+                soundEvents[i].soundEventListener = new SoundVoidEventListener();
+                soundEvents[i].soundEventListener.GameEvent = soundEvents[i].triggerEvent;
+                soundEvents[i].soundEventListener.SoundEventClass = soundEvents[i];
+                soundEvents[i].soundEventListener.Enable();
+                soundEvents[i].SetUpEvent(i);
+                soundEvents[i].audioSource = thisAudioSource;
+
+                if (soundEvents[i].runOnStart)
+                    soundEvents[i].EventRaised(0);
+            }
+    }
+
+    public void StartCoroutine(int id)
+    {
+        if (soundEvents[id] != null)
+            StartCoroutine(soundEvents[id].WaitForDelay());
+    }
+
+    public void ExternalRaiseAll()
+    {
+        for (int i = 0; i < soundEvents.Length; i++)
+        {
+            soundEvents[i].EventRaised(0);
+        }
+    }
+
+    public void ExternalRaiseAtIndex(int index)
+    {
+        if (index >= 0 && index < soundEvents.Length)
+            soundEvents[index].EventRaised(0);
+    }
+}
+
+[System.Serializable]
+public class SoundEvent
+{
+    #region Class Variables
+    [HideInInspector] public AudioManager thisAudioManager;
+    [HideInInspector] public AudioClip audioClip;
+    [HideInInspector] public AudioSource audioSource;
+    [HideInInspector] public int soundEventId;
+    [HideInInspector] public SoundVoidEventListener soundEventListener;
+    [HideInInspector] public float triggerDelay = 0.0f;
+    [HideInInspector] public VoidEvent triggerEvent;
+    [HideInInspector] public bool runOnce, runOnStart, loopSound;
+
+    private bool eventFired = false;
+    private float parsedValue = 0;
+    private WaitForSeconds triggerDelayWait; 
+    #endregion
+
+    public void SetUpEvent(int id)
+    {
+        triggerDelayWait = new WaitForSeconds(triggerDelay);
+        soundEventId = id;
+    }
+
+    public void ResetEventFired()
+    {
+        eventFired = false;
+    }
+
+    public void EventRaised(float value)
+    {
+        parsedValue = value;
+
+        if (!eventFired)
+        {
+            if (triggerDelay <= 0)
+                PlaySound();
+            else if (triggerDelay > 0)
+                thisAudioManager.StartCoroutine(soundEventId);
+            // Co-routines must be started from mono-behaviour class
+        }
+
+        if (runOnce)
+        {
+            eventFired = true;
+        }
+    }
+
+    private void PlaySound()
+    {
+        if (audioClip == null)
+            return;
+
+        audioSource.loop = loopSound;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        Debug.Log("Playing Sound: " + audioClip.name);
+    }
+
+    public IEnumerator WaitForDelay()
+    {
+        yield return triggerDelayWait;
+        PlaySound();
+    }
+}
+
+    #region SoundEventListener
+    public abstract class SoundEventListener<T, E, SE> :
+    IGameEventListener<T> where E : BaseGameEvent<T> where SE : SoundEvent
+    {
+        [SerializeField] private E gameEvent;
+        [SerializeField] private SE soundEventClass;
+
+        public E GameEvent
+        {
+            get { return gameEvent; }
+            set { gameEvent = value; }
+        }
+
+        public SE SoundEventClass
+        {
+            get { return soundEventClass; }
+            set { soundEventClass = value; }
+        }
+
+        public void Enable()
+        {
+            if (gameEvent == null)
+            {
+                return;
+            }
+
+            GameEvent.RegisterListener(this);
+        }
+
+        public void Disable()
+        {
+            if (gameEvent == null)
+            {
+                return;
+            }
+
+            GameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised(T item)
+        {
+            if (item.GetType() == typeof(Void))
+            {
+                SoundEventClass.EventRaised(0);
+            }
+            else if (item.GetType() == typeof(float))
+            {
+                // Currently disabled, no need for float events for now
+                float tempFloat = float.Parse(item.ToString());
+                // SoundEventClass.EventRaised(tempFloat);
             }
         }
     }
 
-    public void Play(string sound)
-	{
-		Sound s = Array.Find(sounds, item => item.name == sound);
-
-        if (s == null)
-		{
-			Debug.LogWarning("Sound: " +name + " not found!");
-			return;
-		}
-
-        s.source.volume = s.volume;
-        s.source.pitch = s.pitch;
-
-		s.source.Play();
-	}
-
-    public void DelayedPlay(string sound)
+    public class SoundVoidEventListener : SoundEventListener<Void, VoidEvent, SoundEvent>
     {
-        currentTime = Time.time;
-        // delayTime = seconds;
-        delayedSound = sound;
-        timerActive = true;
     }
-
-}
+    #endregion
---
+++ MazeGeneration/Assets/Scripts/Audio/AudioManager_Inspector.cs
@@ -0,0 +1,67 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AudioManager))]
+public class AudioManager_Inspector : UnityEditor.Editor
+{
+    private GUIStyle headerStyle;
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Audio Manager by Jakob Husted", MessageType.None);
+
+        var script = target as AudioManager;
+
+        DrawDefaultInspector(); // for other non-HideInInspector fields
+
+        if (script.soundEvents != null)
+            for (int i = 0; i < script.soundEvents.Length; i++)
+            {
+                DrawUILine(false);
+
+                SerializedProperty triggerEventProp = serializedObject.FindProperty("soundEvents.Array.data[" + i + "].triggerEvent");
+                EditorGUILayout.PropertyField(triggerEventProp);
+
+                SerializedProperty audioClipProp = serializedObject.FindProperty("soundEvents.Array.data[" + i + "].audioClip");
+                EditorGUILayout.PropertyField(audioClipProp);
+
+                script.soundEvents[i].runOnce = EditorGUILayout.Toggle("Run Once?", script.soundEvents[i].runOnce);
+                script.soundEvents[i].runOnStart = EditorGUILayout.Toggle("Run On Start?", script.soundEvents[i].runOnStart);
+                script.soundEvents[i].loopSound = EditorGUILayout.Toggle("Loop Sound?", script.soundEvents[i].loopSound);
+
+                script.soundEvents[i].triggerDelay =
+                    EditorGUILayout.FloatField("Trigger Delay", script.soundEvents[i].triggerDelay);
+
+                DrawUILine(false);
+
+                serializedObject.ApplyModifiedProperties();
+
+                if (GUI.changed)
+                {
+                    EditorUtility.SetDirty(script);
+                }
+            }
+    }
+
+    #region DrawUILine function
+    public static void DrawUILine(bool start)
+    {
+        Color color = new Color(1, 1, 1, 0.3f);
+        int thickness = 1;
+        if (start)
+            thickness = 7;
+        int padding = 8;
+
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+        r.height = thickness;
+        r.y += padding / 2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
+    }
+    #endregion
+}
+#endif
