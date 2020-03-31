c64602f3d20531f18bd4eae19d3cd236368df811
Heraji <jakob.hust@gmail.com>
2020-03-31 11:22:45 +0200

    Fixes to event system

--- MazeGeneration/Assets/Scripts/Audio/EventSystem/VoidEventDifPath.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Event", menuName = "Game Events/Void Event")]
-public class VoidEventDifPath : BaseGameEvent<Void>
-{
-    public void Raise() => Raise(new Void());
-}
--- MazeGeneration/Assets/Scripts/Interactable/TestCompanion.cs
+++ MazeGeneration/Assets/Scripts/Interactable/TestCompanion.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class TestCompanion : MonoBehaviour
 {
     public GameObject companionHead;
+    private GameObject mainCamObj;
+
+    private void Start()
+    {
+        if (companionHead != null)
+            mainCamObj = Camera.main.gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        if (mainCamObj == null)
+            return;
 
 
+    }
 }
