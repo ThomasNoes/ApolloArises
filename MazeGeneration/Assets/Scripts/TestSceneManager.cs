d40417b47f06e89a25800639755b0b533fb25e2b
Heraji <jakob.hust@gmail.com>
2020-04-01 15:37:09 +0200

    Updates

--- MazeGeneration/Assets/Scripts/TestSceneManager.cs
+++ MazeGeneration/Assets/Scripts/TestSceneManager.cs
@@ -1,30 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestSceneManager : MonoBehaviour
 {
     public GameObject companion1, companion2;
+    private TestCompanion companion1Script, companion2Script;
     private GameObject camObj;
+    public DataLogger dataLogger;
+    public int testSceneIndexFrom = 1, testSceneIndexTo = 3;
+
+    private int currentSceneIndex = 0;
 
     private void Start()
     {
-        // TODO: start logging time, start logging wall hits
-
+        dataLogger?.LogTimeStart();
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         camObj = Camera.main.gameObject;
 
-        if (camObj != null && companion1 != null && companion2 != null)
+        if (PlayerPrefs.GetInt("TestRunning") != 1)
         {
+            PlayerPrefs.SetInt("TestRunning", 1);
+
+            ScenesVisitedContainer SVC = new ScenesVisitedContainer();
+            SVC.sceneVisited = new bool[3];
+            PlayerPrefs.SetString("ScenesVisited", JsonUtility.ToJson(SVC));
+        }
+
+            if (camObj != null && companion1 != null && companion2 != null)
+        {
+            companion1Script = companion1.GetComponent<TestCompanion>();
+            companion2Script = companion2.GetComponent<TestCompanion>();
+
+            if (companion1Script == null || companion2Script == null)
+                return;
+
             if (Vector3.Distance(camObj.transform.position, companion1.transform.position) > Vector3.Distance(camObj.transform.position, companion2.transform.position))
             {
-                // TODO: choose which companion to be enabled at start
+                companion1Script.EnableMesh();
+                companion2Script.DisableMesh();
+            }
+            else
+            {
+                companion1Script.DisableMesh();
+                companion2Script.EnableMesh();
             }
         }
     }
 
     public void NextSceneRandom()
     {
-        Debug.Log("Next random scene!");
+        return;
+
+        ScenesVisitedContainer SVC = new ScenesVisitedContainer();
+
+        string dataString = PlayerPrefs.GetString("ScenesVisited");
+        SVC = JsonUtility.FromJson<ScenesVisitedContainer>(dataString);
+
+        if (SVC == null)
+            return;
+
+        int index = Random.Range(testSceneIndexFrom, testSceneIndexTo);
+
+        if (SVC.sceneVisited != null)
+        {
+            for (int i = 0; i < SVC.sceneVisited.Length; i++)
+            {
+                if (index >= 0 && index < SVC.sceneVisited.Length)
+                {
+
+                }
+            }
+        }
     }
 
     public void NextSceneAtIndex(int index)
@@ -36,4 +84,12 @@ public class TestSceneManager : MonoBehaviour
     {
 
     }
+}
+
+public class ScenesVisitedContainer
+{
+    /// <summary>
+    /// Index 0: is walled condition, 1: windows, 2: open
+    /// </summary>
+    public bool[] sceneVisited;
 }
