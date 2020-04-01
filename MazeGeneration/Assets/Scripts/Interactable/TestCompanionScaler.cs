91d617a2d98f2080a583d2df9c4ed2683908b2d7
Heraji <jakob.hust@gmail.com>
2020-04-01 09:18:16 +0200

    update

---
+++ MazeGeneration/Assets/Scripts/Interactable/TestCompanionScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class TestCompanionScaler : MonoBehaviour
+{
+    public string tileId;
+    public GameObject mainScreen;
+    private GameObject tileObj;
+
+    public void Start()
+    {
+        if (String.IsNullOrEmpty(tileId))
+            return;
+
+        tileObj = GameObject.Find("Tile " + tileId);
+
+        if (tileObj != null)
+        {
+            transform.position = tileObj.transform.position;
+        }
+    }
+}
