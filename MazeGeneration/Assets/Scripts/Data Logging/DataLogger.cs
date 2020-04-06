41485cf1390c63467c5a564374c4c5be01fc98db
noesmos <tnm@live.dk>
2020-04-06 11:44:08 +0200

    update scenes

--- MazeGeneration/Assets/Scripts/Data Logging/DataLogger.cs
+++ MazeGeneration/Assets/Scripts/Data Logging/DataLogger.cs
@@ -199,7 +199,7 @@ public class DataLogger : MonoBehaviour
     /// </summary>
     public void PostDataOnline()
     {
-        //Debug.Log("Posting Online");
+        Debug.Log("Posting Online");
         if (dataHandler == null)
             return;
 
