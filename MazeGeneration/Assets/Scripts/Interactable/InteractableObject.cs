a60db72e27ac6ad6112e2fceb1b5cc9715def1bc
Heraji <jakob.hust@gmail.com>
2020-04-08 10:11:24 +0200

    Can now throw things through portals

--- MazeGeneration/Assets/Scripts/Interactable/InteractableObject.cs
+++ MazeGeneration/Assets/Scripts/Interactable/InteractableObject.cs
@@ -1,7 +1,11 @@
 ﻿namespace Assets.Scripts.Interactable
 {
     using UnityEngine;
+    using System.Collections;
+
+#if UNITY_EDITOR
     using UnityEditor;
+#endif
 
     [RequireComponent(typeof(OVRGrabbable), typeof(Rigidbody))]
     public class InteractableObject : MonoBehaviour
@@ -11,10 +15,12 @@
         private GameObject thisObjCopy, mainObj, rightHandObj, leftHandObj;
         private bool copyExist, activated;
         [HideInInspector] public bool isParentObject = true, isInHand;
+        private WaitForSeconds delay;
 
         private void Start()
         {
             renderController = FindObjectOfType<PortalRenderController>();
+            delay = new WaitForSeconds(1.0f);
             Invoke("DelayedStart", 0.5f);
         }
 
@@ -86,48 +92,43 @@
                 if (!isInHand)
                 {
                     if (col.CompareTag("PortalGroundCol"))
-                    {          
-                        if (copyExist && thisObjCopy != null)
-                        {
-                            Destroy(thisObjCopy);
-                            copyExist = false;
-                        }
+                    {
+                        StopAllCoroutines();
 
-                        CopySpawner(col.transform.parent.gameObject.GetComponent<NewTeleporter>().isForwardTeleporter ? true : false, col);
+                        if (thisObjCopy == null)
+                            CopySpawner(col.transform.parent.gameObject.GetComponent<NewTeleporter>().isForwardTeleporter ? true : false, col);
                     }
                     if (col.CompareTag("PortalRenderCol"))
                     {
+                        StopAllCoroutines();
+
                         activated = false;
                         thisObjCopy.transform.position = transform.position;
                         transform.Translate(offsetVector, Space.World);
                         UpdateOffset(col.transform.parent.gameObject.GetComponent<NewTeleporter>().isForwardTeleporter);
-                        Debug.Log("TELEPORTED");
+                        activated = true;
                     }
                 }
                 else
                 {
                     // TODO
                 }
-
-            //if (activated)
-            //    if (!isInHand)
-            //        if (col.CompareTag("PortalRenderCol"))
-            //        {
-            //            CopySpawner(col.transform.parent.gameObject.GetComponent<NewTeleporter>().isForwardTeleporter ? true : false, col);
-            //        }
         }
 
         private void OnTriggerExit(Collider col)
         {
-            //if (activated)
-            //{
-            //    if (!isInHand && isParentObject)
-            //        if (col.CompareTag("PortalRenderCol"))
-            //        {
-            //            if (thisObjCopy != null)
-            //                Destroy(thisObjCopy);
-            //        }
-            //}
+            if (col.CompareTag("PortalGroundCol"))
+            {
+                StartCoroutine(InColliderCheck());
+            }
+        }
+
+        private IEnumerator InColliderCheck()
+        {
+            yield return delay;
+
+            if (thisObjCopy != null)
+                Destroy(thisObjCopy);
         }
 
         /// <param name="dir">false: prev, true: next</param>
@@ -140,8 +141,6 @@
                 offsetVector = renderController.GetCurrentPos() - renderController.GetNextMazePos();
             else
                 offsetVector = renderController.GetCurrentPos() - renderController.GetPrevMazePos();
-
-            activated = true;
         }
 
         public void SetParentObject(GameObject parentObj)
