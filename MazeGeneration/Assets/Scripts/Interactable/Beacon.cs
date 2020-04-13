30eaf23c2bd5a7d00c32de81863d4f6c120e3232
Heraji <jakob.hust@gmail.com>
2020-04-13 15:58:01 +0200

    Update

---
+++ MazeGeneration/Assets/Scripts/Interactable/Beacon.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beacon : MonoBehaviour
+{
+    public bool isActive;
+    public Beacon connectedToBack, connectedToForward;
+    private BeaconManager beaconManager;
+
+    private void Start()
+    {
+        beaconManager = GetComponent<BeaconManager>();
+
+        if (beaconManager == null)
+            beaconManager = FindObjectOfType<BeaconManager>();
+
+        if (beaconManager != null)
+            beaconManager.beacons.Add(this);
+    }
+
+    public void LightBeacon()
+    {
+        Debug.Log("Beacon on " + gameObject.name + " is lit!");
+        // TODO turn on some effect
+    }
+}
---
+++ MazeGeneration/Assets/Scripts/Interactable/BeaconManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class BeaconManager : MonoBehaviour
+{
+    public List<Beacon> beacons = new List<Beacon>();
+
+    private void Start()
+    {
+        
+    }
+
+    public void ConnectNextBeacon()
+    {
+        for (int i = 0; i < beacons.Count; i++)
+        {
+            if (!beacons[i].isActive)
+            {
+                beacons[i].isActive = true;
+                beacons[i].LightBeacon();
+
+                if (i > 0)
+                    ConnectBeam(beacons[i], beacons[i - 1]);
+
+                break;
+            }
+        }
+    }
+
+    private void ConnectBeam(Beacon to, Beacon from)
+    {
+        Debug.Log("Beacon on " + from.gameObject.name + " is connected to " + to.gameObject.name);
+        from.connectedToForward = to;
+        to.connectedToBack = from;
+    }
+
+    public void DisconnectNextBeacon()
+    {
+
+    }
+}
+
+#region CustomInspector
+#if UNITY_EDITOR
+[CustomEditor(typeof(BeaconManager))]
+public class BeaconManager_Editor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var script = target as BeaconManager;
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Connect next beacon!"))
+        {
+            script.ConnectNextBeacon();
+        }
+    }
+}
+#endif
+#endregion
