88d973672e513766da7692d00e37f4d721c16e39
Heraji <jakob.hust@gmail.com>
2020-04-17 13:46:59 +0200

    Added companion prefab

---
+++ MazeGeneration/Assets/Scripts/Interactable/Companion.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Companion : MonoBehaviour
+{
+    public float rotateSpeed = 1.0f, blinkDuration = 0.5f, blinkFrequency = 5.0f;
+    public bool enableBlinking = true, alwaysLookAtPlayer;
+    public GameObject headObj, frameObj;
+    public Image faceImage;
+    public Sprite openEyes, closedEyes;
+
+    private GameObject mainCamObj;
+    private WaitForSeconds blinkDur, blinkFreq;
+
+    private void Start()
+    {
+        if (headObj == null)
+            return;
+
+        mainCamObj = Camera.main.gameObject;
+
+        if (faceImage != null && openEyes != null && closedEyes != null)
+        {
+            StartCoroutine(FaceBehaviour());
+            blinkDur = new WaitForSeconds(blinkDuration);
+            blinkFreq = new WaitForSeconds(blinkFrequency);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (mainCamObj == null || !alwaysLookAtPlayer)
+            return;
+
+        Vector3 targetDirection = mainCamObj.transform.position - headObj.transform.position;
+        float singleStep = rotateSpeed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(headObj.transform.forward, targetDirection, singleStep, 0.0f);
+
+        headObj.transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    private IEnumerator FaceBehaviour()
+    {
+        while (enableBlinking)
+        {
+            faceImage.sprite = closedEyes;
+
+            yield return blinkDur;
+
+            faceImage.sprite = openEyes;
+
+            yield return blinkFreq;
+        }
+    }
+}
