b9a58578d4f1c7f4b3aa5ebb5f1f8030a90e5d99
noesmos <tnm@live.dk>
2020-04-17 15:10:41 +0200

    rotating clouds

---
+++ MazeGeneration/Assets/CloudRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudRotation : MonoBehaviour
+{
+
+    public float Speed =1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeChildrenFaceCenter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Speed * Time.deltaTime);
+    }
+
+    void MakeChildrenFaceCenter()
+    {
+        int children = transform.childCount;
+        for (int i = 0; i < children; ++i)
+        {
+            Vector3 pos = transform.GetChild(i).position;
+            pos.y = transform.position.y;
+
+            Vector3 toCenter = pos - transform.position;
+
+            transform.GetChild(i).forward = toCenter; 
+        }
+
+    }
+}
