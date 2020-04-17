7c138a0733e1bb05683e4a927a47fdad28ccdac2
Heraji <jakob.hust@gmail.com>
2020-04-17 12:07:52 +0200

    Added sound to beacons

--- MazeGeneration/Assets/Scripts/Interactable/Beacon.cs
+++ MazeGeneration/Assets/Scripts/Interactable/Beacon.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Beacon : MonoBehaviour
 {
     public bool isActive;
     public Beacon connectedToBack, connectedToForward;
     public GameObject orb;
+    public AudioClip closingRoof, orbOn;
+
     private Renderer orbRenderer;
     private Animator animator;
     private BeaconManager beaconManager;
     private Color startColor, endColor;
+    private AudioSource audioSource;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         beaconManager = GetComponent<BeaconManager>();
+        audioSource = GetComponent<AudioSource>();
 
         if (beaconManager == null)
             beaconManager = FindObjectOfType<BeaconManager>();
@@ -38,11 +43,28 @@ public class Beacon : MonoBehaviour
     {
         //Debug.Log("Beacon on " + gameObject.name + " is lit!");
         animator?.SetBool("Close", true);
+        StartCoroutine(PlayBeaconSoundWithDelay());
 
         if (orbRenderer != null && beaconManager != null)
         {
             orbRenderer.material.EnableKeyword("_EMISSION");
             orbRenderer.material.SetColor("_EmissionColor", beaconManager.orbEndEmission);
         }
+    }
+
+    private IEnumerator PlayBeaconSoundWithDelay()
+    {
+        PlaySound(closingRoof);
+        yield return new WaitForSeconds(6.0f);
+        PlaySound(orbOn);
+    }
+
+    private void PlaySound(AudioClip audioClip)
+    {
+        if (audioClip == null || audioSource == null)
+            return;
+
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
 }
