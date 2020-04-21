b19da25487df7a6cc0ea2a461cade74e8299b13a
noesmos <tnm@live.dk>
2020-04-21 14:41:48 +0200

    Update CompanionPathFinding.cs

--- MazeGeneration/Assets/Scripts/CompanionPathFinding.cs
+++ MazeGeneration/Assets/Scripts/CompanionPathFinding.cs
@@ -293,23 +293,3 @@ public class CompanionPathFinding : MonoBehaviour
 
 }
 
-#if UNITY_EDITOR
-[CustomEditor(typeof(CompanionPathFinding))]
-public class Companion_Inspector : UnityEditor.Editor
-{
-    private GUIStyle headerStyle;
-
-    public override void OnInspectorGUI()
-    {
-        var script = target as CompanionPathFinding;
-
-        DrawDefaultInspector(); // for other non-HideInInspector fields
-
-        if (GUILayout.Button("Go to Target"))
-        {
-            Debug.Log("pressing button");
-            script.Update();
-        }
-    }
-}
-#endif
