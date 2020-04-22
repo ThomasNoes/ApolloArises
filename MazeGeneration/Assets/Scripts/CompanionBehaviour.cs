fe35c4567d481eaee922a2e1489515afaeeeed9f
noesmos <tnm@live.dk>
2020-04-22 14:58:01 +0200

    dialog reader expanded

--- MazeGeneration/Assets/Scripts/CompanionBehaviour.cs
+++ MazeGeneration/Assets/Scripts/CompanionBehaviour.cs
@@ -7,12 +7,20 @@ using UnityEditor;
 
 public class CompanionBehaviour : MonoBehaviour
 {
+    //this script manages the companions behaviour
+    //like what tile to move to and when
+    //what to say and when to say
+    //and generally listen to events that happens in the game, and what player actions happens
+
+    public DialogReader dr;
+    public CompanionPathFinding cpf;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        dr = GetComponent<DialogReader>();
+        cpf = GetComponent<CompanionPathFinding>();
     }
 
     // Update is called once per frame
@@ -21,27 +29,43 @@ public class CompanionBehaviour : MonoBehaviour
         
     }
 
+    //start 
 
 
 }
 
+
+
+
+
+
+
+
+
+
 #if UNITY_EDITOR
-[CustomEditor(typeof(CompanionPathFinding))]
+[CustomEditor(typeof(CompanionBehaviour))]
 public class Companion_Inspector : UnityEditor.Editor
 {
     private GUIStyle headerStyle;
 
     public override void OnInspectorGUI()
     {
-        var script = target as CompanionPathFinding;
+        var script = target as CompanionBehaviour;
 
         DrawDefaultInspector(); // for other non-HideInInspector fields
 
         if (GUILayout.Button("Go to Target"))
         {
-            Debug.Log("pressing button");
-            script.Update();
+            Debug.Log("Go To Target Pressed");
+            script.cpf.GoToTarget();
         }
+        if (GUILayout.Button("Display Dialog"))
+        {
+            Debug.Log("Display Dialog Pressed");
+            script.dr.DisplayDialog();
+        }
+
     }
 }
 #endif
