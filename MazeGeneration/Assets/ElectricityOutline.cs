b6c37d3910c72218f1eb666e7488459ae416a7d8
noesmos <tnm@live.dk>
2020-04-30 10:45:12 +0200

    tower rail

---
+++ MazeGeneration/Assets/ElectricityOutline.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityOutline : MonoBehaviour
+{
+    public Transform[] points;
+
+    LineRenderer lr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+        lr.positionCount = points.Length;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            lr.SetPosition(i, points[i].position);
+        }
+
+
+    }
+
+}
