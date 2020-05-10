07c6e0ebae27bca2121928a7f1b21428cd0427ff
noesmos <tnm@live.dk>
2020-05-10 12:12:08 +0200

    3d sound

---
+++ MazeGeneration/Assets/sounCollsion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounCollsion : MonoBehaviour
+{
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+
+    }
+}
